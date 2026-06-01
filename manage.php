9bf61d966eaf28b65cc99063698cdb3adce5d3ec
Sreetoma Deb Roy <106601739@student.swin.edu.au>
2026-06-01 11:08:35 +1000

    Added head, meta tags and login/logout validation

--- manage.php
+++ manage.php
@@ -1,16 +1,34 @@
-<?php include 'header.inc'; ?>
-<?php
-    session_start();
-    // If not logged in, redirect to login page 
-    if (!isset($_SESSION['username'])) {
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Manager Dashboard for Ecosolutions">
+    <meta name="author" content="Sreetoma Deb Roy, WWW(Worldwide Women)">
+    <title>Manager - Manage EOIs</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+    <?php include 'header.inc'; ?>
+    <?php
+        session_start();
+        // If not logged in, redirect to login page 
+        if (!isset($_SESSION['username'])) {
+            header("Location: login.php");
+            exit();
+        }
+
+        //logout by destroying session
+        if (isset($_GET['logout'])) {
+        session_unset();
+        session_destroy();
         header("Location: login.php");
         exit();
-    }
+        }
+    ?>    
 
-    //logout by destroying session
-    if (isset($_GET['logout'])) {
-    session_unset();
-    session_destroy();
-    header("Location: login.php");
-    exit();
-    }   
+
+    <?php include 'footer.inc'; ?>
+
+</body>
+</html>
