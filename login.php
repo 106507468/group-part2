ece320dd8e482307a17ce6f35644848ead84f3ce
Sreetoma Deb Roy <106601739@student.swin.edu.au>
2026-06-01 10:32:47 +1000

    Added form for login page

--- login.php
+++ login.php
@@ -34,6 +34,20 @@
         }
     }
 ?>
+    <h1>HR Manager Login</h1>
+
+    <form action="login.php" method="post">
+        <p>
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" required>
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+        </p>
+        <input type="submit" value="Login">
+    </form>
+
 
 <?php include 'footer.inc'; ?>
 
