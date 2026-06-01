f9f5bf363e3432e6f8485cfa1879e94b1e60c1f5
Sreetoma Deb Roy <106601739@student.swin.edu.au>
2026-06-01 10:05:27 +1000

    Included head with meta tages, header.inc and footer.inc

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Login for Ecosolutions">
+    <meta name="author" content="Sreetoma Deb Roy, WWW(Worldwide Women)">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<?php include 'header.inc'; ?>
+
+
+<?php include 'footer.inc'; ?>
+
+</body>
+</html>
